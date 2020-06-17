69d0b4983426761f05d105b1399a680442192699
Kristiina Piip <kristiina.piip@gmail.com>
2020-06-17 15:36:39 +0200

    Updates

---
+++ precache-manifest.a2c7364655a39e0246a854a04f17fb76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d432289159d4761bd779837a354f6c",
+    "url": "/cyf-hotel-react/index.html"
+  },
+  {
+    "revision": "ebf92f97e5a6f06190fe",
+    "url": "/cyf-hotel-react/static/css/main.c2ca8abf.chunk.css"
+  },
+  {
+    "revision": "6cbda47a83af6726318f",
+    "url": "/cyf-hotel-react/static/js/2.6b777414.chunk.js"
+  },
+  {
+    "revision": "ebf92f97e5a6f06190fe",
+    "url": "/cyf-hotel-react/static/js/main.04b3519e.chunk.js"
+  },
+  {
+    "revision": "34311b7f62da8b8a0d63",
+    "url": "/cyf-hotel-react/static/js/runtime~main.dc6ae9fc.js"
+  }
+]);
